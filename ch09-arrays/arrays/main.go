e16fea361272bfbad6ca3aa7ffe82bd1e82907e9
Gunish Sharma <gunishsharma20@gmail.com>
2026-08-18 16:17:25 -0600

    start slices

--- ch09-arrays/arrays/main.go
+++ ch09-arrays/arrays/main.go
@@ -1,6 +1,11 @@
 package main
 
 func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
-	// ?
+	cost1 := len(primary)
+	cost2 := len(primary) + len(secondary)
+	cost3 := len(primary) + len(secondary) + len(tertiary)
+	m := [3]string{primary, secondary, tertiary}
+	c := [3]int{cost1, cost2, cost3}
+	return m, c 
 }
 
---
+++ ch09-arrays/slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"errors"
+)
+
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
+	// ?
+}
+
